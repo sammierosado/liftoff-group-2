1bd12eae4f33025824339ae6ed9a70c96d5e0cec
cameronfeeman <139170459+cameronfeeman@users.noreply.github.com>
2023-12-12 15:17:52 -0500

    Added Home Page

---
+++ adventure-appraisals-react/src/components/homePageDescription.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+let logo = "Adventure Appraisals"
+
+const HomePage = () => {
+return (
+<div>
+<header>
+<h1> Welcome to Adventure Appraisals </h1>
+</header
+)
+
+}
