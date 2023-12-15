81a5e25e65a23e92e701ebabbf75fc83aac85bfd
Justin Wright <djw6497@gmail.com>
2023-12-15 15:21:11 -0600

    Updated linking for Itineraries in the NavigationBar to link to the server side URL to connect to Thymeleaf template.

--- adventure-appraisals-react/src/components/HomePage.js
+++ adventure-appraisals-react/src/components/HomePage.js
@@ -4,11 +4,11 @@ import NavigationBar from './NavigationBar';
 let logo = "Adventure Appraisals"
 
 const HomePage = () => {
-return (
-<div>
-<h1> Welcome to Adventure Appraisals </h1>
-</div>
-
-)};
+    return (
+        <div>
+            <h1> Welcome to Adventure Appraisals </h1>
+        </div>
+    );
+};
 
 export default HomePage;
