ef8afe201059c7a39617158bd337d806704d59d9
cameronfeeman <139170459+cameronfeeman@users.noreply.github.com>
2024-01-18 16:23:00 -0500

    Updated merge conflict for review page -labeled pt because pt1 wouldn't delete

--- adventure-appraisals-react/src/components/ReviewPage.js
+++ adventure-appraisals-react/src/components/ReviewPage.js
@@ -2,8 +2,6 @@ import { Button } from "@mui/base";
 import { UserAuth } from "../Context/AuthContext";
 import { useEffect, useState } from "react";
 import { Box, Container, Paper, Rating, TextField } from "@mui/material";
-import WeatherApi from "./WeatherApi"
-
 
 const ReviewPage = () => {
     const { user } = UserAuth();
@@ -13,26 +11,23 @@ const ReviewPage = () => {
     const [ name, setName ] = useState();
     const [ rating, setRating ] = useState();
     const [ itinerary, setItinerary ] = useState({});
+    const [ favorite, setFavorite ] = useState(false);
 
     const [ reviews, setReviews ] = useState();
 
     var url = window.location.pathname;
     const itineraryId = Number(url.substring(url.lastIndexOf('/') + 1));
-    const userEmail = user?.email;
-
-
-useEffect(() => {
-    fetch(`http://localhost:8080/itineraries/itinerary/${itineraryId}`)
-    .then(res => res.json())
-    .then(result => {
-    setItinerary(result);
-
-    });
-}, []);
-
+    const userEmail = user ? user.email : null;
 
     useEffect(() => {
-        setReviews(itinerary.reviews);
+        userEmail && fetch(`http://localhost:8080/userfavorites/${userEmail}`).then(res => res.json()).then(result => setFavorite(result.includes(itineraryId))).catch(error => null);
+        fetch(`http://localhost:8080/itineraries/itinerary/${itineraryId}`).then(res => res.json()).then(result => setItinerary(result));
+    }, [user]);
+
+    useEffect(() => {
+        if (itinerary?.reviews?.length !== 0) {
+            setReviews(itinerary.reviews);
+        }
     }, [itinerary]);
 
     const handleSubmit = (e) => {
@@ -47,11 +42,35 @@ useEffect(() => {
         setName('');
     };
 
+    const handleFavorite = (e) => {
+        e.preventDefault();
+        fetch(`http://localhost:8080/userfavorites/add/${userEmail}`, {
+            method:"POST",
+            headers:{'Content-Type':"application/json"},
+            body:JSON.stringify(itinerary)
+        });
+        setFavorite(true);
+    }
+
+    const handleUnfavorite = (e) => {
+        e.preventDefault();
+        fetch(`http://localhost:8080/userfavorites/remove/${userEmail}`, {
+            method:"POST",
+            headers:{'Content-Type':"application/json"},
+            body:JSON.stringify(itinerary)
+        });
+        setFavorite(false);
+    }
+
     return (
         <Container>
+            {(favorite && user) ? <Button onClick={handleUnfavorite}>Unfavorite?</Button>
+            : (user) ? <Button onClick={handleFavorite}>Favorite?</Button>
+            : null
+            }
             {(user) ? (
                 <Paper elevation={3} style={paperStyle}>
-                <h1>Add Review</h1>
+                    <h1>Add Review</h1>
                     <Box
                         component="form"
                         sx={{
@@ -71,7 +90,6 @@ useEffect(() => {
                     <h1>Sign in to add a new review</h1>
                 </Paper>
             )}
-
             <Paper elevation={3} style={paperStyle}>
                 <h1>Reviews</h1>
                 <br />
